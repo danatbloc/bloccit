efccbaf6976352e005b8a3a5c76c39b2c8db3567
danielfsokol <danielfsokol@gmail.com>
2018-01-25 09:29:36 -0500

    complete crud-assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:created_at)
+
+    @posts.first.update_attribute("title", "SPAM")
+    @posts.each.with_index do |post,i|
+      if i % 5 == 0
+        post.update_attribute("title", "SPAM")
+      end
+    end
   end
 
   def show
