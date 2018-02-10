4134ef334a4242dbd2d031924a4dd533029c35a1
danielfsokol <danielfsokol@gmail.com>
2018-02-10 14:27:22 -0500

    complete authorization checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,11 +71,19 @@ require 'random_data'
    )
  end
 
- user = User.first
- user.update_attributes!(
-  email: "dsokol1@aol.com",
-  password: 'helloworld'
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
  )
+ 
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+
 
  puts "Seed finished"
  puts "#{User.count} users created"
